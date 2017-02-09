5bc20a0de57f440f45fdd459f42a0933d3ac18b6
Kenneth Ellis McCall <xellisx@gmail.com>
2017-02-08 18:24:51 -0600

    Got rid of erronous row.

--- epsiom.sql
+++ epsiom.sql
@@ -5,88 +5,83 @@
 -- HeidiSQL Version:             9.4.0.5148
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE DATABASE IF NOT EXISTS `epsiom` /*!40100 DEFAULT CHARACTER SET utf8
-  COLLATE utf8_unicode_ci */;
+
+-- Dumping database structure for epsiom
+CREATE DATABASE IF NOT EXISTS `epsiom` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `epsiom`;
 
+-- Dumping structure for table epsiom.roles
 CREATE TABLE IF NOT EXISTS `roles` (
-  `role_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name`    VARCHAR(50)
-            COLLATE utf8_unicode_ci   DEFAULT '0',
+  `role_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT '0',
   PRIMARY KEY (`role_id`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 6
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
+-- Dumping data for table epsiom.roles: ~3 rows (approximately)
 DELETE FROM `roles`;
-/*!40000 ALTER TABLE `roles`
-  DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
 INSERT INTO `roles` (`role_id`, `name`) VALUES
-  (1, 'Developer'),
-  (2, 'Admin'),
-  (3, 'User');
-/*!40000 ALTER TABLE `roles`
-  ENABLE KEYS */;
+	(1, 'Developer'),
+	(2, 'Admin'),
+	(3, 'User');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 
-
+-- Dumping structure for table epsiom.role_roles
 CREATE TABLE IF NOT EXISTS `role_roles` (
-  `role_id1` INT(10) UNSIGNED NOT NULL
-  COMMENT 'Main role',
-  `role_id2` INT(10) UNSIGNED NOT NULL
-  COMMENT 'Role it can access',
+  `role_id1` int(10) unsigned NOT NULL COMMENT 'Main role',
+  `role_id2` int(10) unsigned NOT NULL COMMENT 'Role it can access',
   KEY `role_id1` (`role_id1`),
   KEY `role_id2` (`role_id2`),
   CONSTRAINT `role_id1` FOREIGN KEY (`role_id1`) REFERENCES `roles` (`role_id`),
   CONSTRAINT `role_id2` FOREIGN KEY (`role_id2`) REFERENCES `roles` (`role_id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
+-- Dumping data for table epsiom.role_roles: ~4 rows (approximately)
 DELETE FROM `role_roles`;
-/*!40000 ALTER TABLE `role_roles`
-  DISABLE KEYS */;
+/*!40000 ALTER TABLE `role_roles` DISABLE KEYS */;
 INSERT INTO `role_roles` (`role_id1`, `role_id2`) VALUES
-  (1, 2),
-  (1, 3),
-  (1, 4),
-  (2, 3);
-/*!40000 ALTER TABLE `role_roles`
-  ENABLE KEYS */;
+	(1, 2),
+	(1, 3),
+	(2, 3);
+/*!40000 ALTER TABLE `role_roles` ENABLE KEYS */;
 
+-- Dumping structure for table epsiom.sessions
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) unsigned NOT NULL,
+  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table epsiom.sessions: ~0 rows (approximately)
+DELETE FROM `sessions`;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+
+-- Dumping structure for table epsiom.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id`  INT(10) UNSIGNED        NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(50)
-             COLLATE utf8_unicode_ci NOT NULL,
-  `password` VARCHAR(255)
-             COLLATE utf8_unicode_ci NOT NULL,
-  `role_id`  INT(10) UNSIGNED        NOT NULL,
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`user_id`),
   KEY `user_role_id` (`role_id`),
   CONSTRAINT `user_role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- Dumping data for table epsiom.users: ~0 rows (approximately)
 DELETE FROM `users`;
-/*!40000 ALTER TABLE `users`
-  DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`user_id`, `username`, `password`, `role_id`) VALUES
-  (1, 'admin', '$2a$10$E6XVON4nRXTbsZyoUMmKyuwR6bfn2YZkOek//QWuQJVw.8VxVCNR.', 2);
-/*!40000 ALTER TABLE `users`
-  ENABLE KEYS */;
+	(1, 'admin', '$2a$10$E6XVON4nRXTbsZyoUMmKyuwR6bfn2YZkOek//QWuQJVw.8VxVCNR.', 2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE = IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS = IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
